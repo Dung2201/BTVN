87550f261a2dd9ebd941cac09d98823b37cfb79c
Dung2201 <ntdung220197@gmail.com>
2021-08-15 23:13:25 +0900

    update-file

---
+++ Demo-bien/js/my.js
@@ -0,0 +1,5 @@
+function Chuyen_do_F(){
+    var a;
+    a=+document.getElementById('nhietdo').value;
+    document.getElementById('result').innerHTML= (a *1.8) + 32;
+}
---
+++ Demo-bien/js/my1.js
@@ -0,0 +1,11 @@
+/*
+1.Number :2,4
+2.String: "Xin chao" or 'Xin chao'
+3.Null
+4.Array
+5.Object
+6.Boolean
+7.Undefined
+7 kieu du lieu co ban
+bo 4,5 di thi la 5 kieu du lieu nguyen thuy
+*/
